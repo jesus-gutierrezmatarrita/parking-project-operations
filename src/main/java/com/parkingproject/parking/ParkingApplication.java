eed85354f7cdf94b030a1b381a19f1b6af735431
stevenmr98 <37566244+stevenmr98@users.noreply.github.com>
2022-07-22 15:32:47 -0600

    problema con cors resuelto

--- src/main/java/com/parkingproject/parking/ParkingApplication.java
+++ src/main/java/com/parkingproject/parking/ParkingApplication.java
@@ -2,11 +2,24 @@ package com.parkingproject.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ParkingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 }
