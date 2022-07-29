3dcc6f3d0ac8dac4577bd30565a4ac553aad14ec
Jesús Gutiérrez Matarrita <74956389+jesus-gutierrezmatarrita@users.noreply.github.com>
2022-07-29 00:00:37 -0600

    Update ParkingApplication.java

--- src/main/java/com/parkingproject/parking/ParkingApplication.java
+++ src/main/java/com/parkingproject/parking/ParkingApplication.java
@@ -18,7 +18,7 @@ public class ParkingApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://parking-project-ui.herokuapp.com").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
